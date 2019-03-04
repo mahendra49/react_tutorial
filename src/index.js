a356b546c0a244514e010dbd3e7b230cf8d6911e
backslash <mahendrasuthar02@gmail.com>
2019-03-04 15:47:11 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,12 +1,69 @@
-//import react and react-dom
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './seasondisplay'
+ 
 
+class App extends React.Component {
 
-//create a react component
-const App = ()=>{
-	return <div> Hi there </div>;
-}
+	//or just use..or equilanent
 
-//take the component and display it
-ReactDOM.render(<App/> , document.querySelector('#root'));
+	/*
+		without constructor 
+
+		state = {lat : null}
+
+	*/
+
+	constructor(props){
+		super(props);
+
+		//this is only time we do direct assigment
+		//any other time call setState()
+		this.state = { lat : null , errorMsg:"" };
+		
+		
+
+	}
+
+	//life cycle of component after first rendering
+	componentDidMount(){
+		console.log("component mounted");
+		
+
+		//get the geolocation
+		window.navigator.geolocation.getCurrentPosition(
+			(position)=> {
+				//call setstate to update a component
+				//this.state is like properties of class
+				this.setState({lat:position.coords.latitude});
+			},
+			(err)=> {
+				this.setState({errorMsg:"there was some error"});
+			}  	
+		);
+
+	}
+
+	//after any update .. ie after "this.setState()"
+	componentDidUpdate(){
+		console.log("component did update");
+	}
+
+	render(){
+		
+		if(this.state.lat){
+			return <SeasonDisplay lat={this.state.lat}/>;
+		}
+		else if(this.state.errorMsg){
+			return <div>error: {this.state.errorMsg}</div>;
+		}
+		else{
+			return <div> Loading: </div>;
+		}
+				
+
+	}
+
+} 
+
+ReactDOM.render(<App/> , document.querySelector("#root"));
